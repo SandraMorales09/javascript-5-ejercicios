e1d36a301adb69e75cb8de96c20e987e78d57d74
Python-practicas-3-- <114303948+SandraMorales09@users.noreply.github.com>
2025-06-01 23:17:13 -0600

    Delete javascrip 4 ejercicios directory
    Signed-off-by: Python-practicas-3-- <114303948+SandraMorales09@users.noreply.github.com>

--- javascrip 4 ejercicios/Suma de 2 numeros_1.js
+++
@@ -1,5 +0,0 @@
-function suma(a, b) {
-    return a + b;
-}
-
-console.log(suma(2, 3)); // Out: 5
--- javascrip 4 ejercicios/calcula el area de un rectangulo_3.js
+++
@@ -1,7 +0,0 @@
-function areaRectangulo(base, altura) 
-{
-    return base * altura;
-}
-
-console.log(areaRectangulo(4,5)); 
-// Output: 20
